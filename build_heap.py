2132de6ca3c190647b66a77b135701bfaaa56596
Daniels Kikste-Zanders 221RDB373 <125289551+Daniels-221RDB373@users.noreply.github.com>
2023-03-19 17:44:00 +0000

    Done

--- build_heap.py
+++ build_heap.py
@@ -2,11 +2,21 @@
 
 
 def build_heap(data):
+    n =  len (data)
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    for i in range (n // 2, -1, -1):
+        j = i 
+        while 2 * j + 1 < n:
+            k = 2 * j + 1
+            if k + 1 < n and data[k + 1] < data[k]:
+                k += 1
+            if data[j] <= data[k]:
+                break
+            swaps.append((j, k))
+            data[j], data[k] = data[k], data[j]
+            j = k
     return swaps
 
 
@@ -15,11 +25,27 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    input_type = input()
+    if input_type == 'F':
+        file_name = input()
+        if 'a' in file_name:
+            print("Invalid file name.")
+            exit()
+        try:
+            input_file = open("test/" + file_name + "r")
+			n = int(input_file.readline())
+			data = list(map(int, input_file.readline().split()))
+			input_file.close()
+        except:
+            print("Error while reading file.")
+            exit()
+    elif input_type == 'I':
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    else:
+        print("Invalid input type.")
+        return
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
